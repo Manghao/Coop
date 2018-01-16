53bce1862ccb628879696cf43f8391865d14309e
baier2u <geoffrey.baier7@etu.univ-lorraine.fr>
2018-01-16 01:04:29 +0100

    Adding page of show channel's posts

---
+++ src/components/post/Index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>Channel d'id: {{ posts.idChannel }}</p>
+        <div v-if="posts.posts">
+            <div v-for="p in posts.posts">
+                <p>Post: {{ p }}</p>
+            </div>
+        </div>
+        <div v-else>
+            <p>Aucun post pour cette chaîne !</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['posts']
+    }
+</script>
