53c0962f5b4fb8b9f1b4126353247b5b3381326e
Steven W <email@email.fr>
2018-01-22 18:27:06 +0100

    Modifications

--- src/components/app/Index.vue
+++ src/components/app/Index.vue
@@ -1,100 +1,55 @@
 <template>
 	<div>
-		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-			<a class="navbar-brand" href="/">Co'op</a>
-			<button @click="isActive ? isActive = !isActive : null" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
+		<navbar></navbar>
 
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav mr-auto">
-					<li class="nav-item active">
-						<a class="btn btn_channels text-light" @click="isActive = !isActive">Channels</a>
-					</li>
-				</ul>
-				<div class="btn-group mt-1 mb-1" role="group">
-					<button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#account">{{ user.fullname }}</button>
-					<button class="btn btn-danger" @click="logout"><i class="fa fa-sign-out"></i></button>
-				</div>
-			</div>
-		</nav>
 		<div class="container">
-			<channels-list :channels="channels" :isActive="isActive"></channels-list>
-			<posts-list :posts="posts" :members="members"></posts-list>
-		</div>
-
-		<div class="modal fade" id="account" tabindex="-1" role="dialog" aria-labelledby="account" aria-hidden="true">
-			<div class="modal-dialog" role="document">
-				<div class="modal-content">
-					<div class="modal-header">
-						<h5 class="modal-title" id="accountLabel">Mes informations</h5>
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-							<span aria-hidden="true">&times;</span>
-						</button>
-					</div>
-					<div class="modal-body">
-						<p><strong>Nom :</strong> {{ user.fullname }}</p>
-						<p><strong>Email :</strong> {{ user.email }}</p>
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+			<h1 class="mt-3">Liste des channels</h1>
+			<hr />
+			<div class="row">
+				<div v-for="channel in channels" class="card m-3" style="width: 30%;">
+					<div class="card-body">
+						<img class="card-img-top" src="http://via.placeholder.com/350x150" alt="Card image cap">
+						<router-link :to="{ name: 'channel', params: { channel_id: channel._id }}"><h5 class="card-title">{{channel.label}}</h5></router-link>
+						<h6 class="card-subtitle mb-2 text-muted">{{channel.topic}}</h6>
+						<router-link class="nav-link btn btn-primary btn-sm" :to="{ name: 'channel', params: { channel_id: channel._id }}">Ouvrir</router-link>
 					</div>
 				</div>
-			</div>
+			</div>	
 		</div>
 	</div>
 </template>
 
 <script>
-    import { mapGetters } from 'vuex'
-    import ChannelsIndex from '@/components/channel/Index'
-    import PostsIndex from '@/components/post/Index'
+import { mapGetters } from 'vuex'
+import Navbar from '@/components/app/NavBar'
 
-    export default {
-        data () {
-            return {
-                isActive: false
-            }
-        },
-        components: {
-            'channels-list': ChannelsIndex,
-            'posts-list': PostsIndex
-        },
-        created() {
-        	this.$store.dispatch('channel/members')
-            this.$store.dispatch('channel/channels')
-        },
-        computed: {
-            ...mapGetters(
-                {
-                    user: 'auth/getSession',
-                    channels: 'channel/getChannels',
-                    posts: 'channel/getPosts',
-					members: 'channel/getMembers'
-                }
-            )
-        },
-        methods: {
-            logout () {
-                this.$store.dispatch('auth/logout')
-            }
-        }
-    }
+export default {
+	components: {
+		navbar: Navbar
+	},
+	computed: {
+		...mapGetters(
+		{
+			channels: 'channel/getChannels',
+		}
+		)
+	}
+}
 </script>
 
 <style>
-	.slide-fade-enter-active {
-		transition: all .25s ease-in;
-	}
-	.slide-fade-leave-active {
-		transition: all .25s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-	}
-	.slide-fade-enter, .slide-fade-leave-to {
-		transform: translateX(-10px);
-		opacity: 0;
-	}
+.slide-fade-enter-active {
+	transition: all .25s ease-in;
+}
+.slide-fade-leave-active {
+	transition: all .25s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to {
+	transform: translateX(-10px);
+	opacity: 0;
+}
 
-	.btn_channels {
-		cursor: pointer;
-	}
+.btn_channels {
+	cursor: pointer;
+}
 </style>
